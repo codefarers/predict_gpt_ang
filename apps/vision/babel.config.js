927f5394816f7b74638e0c9d083408aca528b665
Romanus Kipngetich Bett <romanus.kipngetich@KEMDGFF6F2.local>
2026-03-04 11:12:48 +0300

    update legacy babel deps

--- apps/vision/babel.config.js
+++ apps/vision/babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ],
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }]
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }]
   ]
 };
