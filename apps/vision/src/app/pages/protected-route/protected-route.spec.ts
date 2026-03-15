5e4640c631e1679042d838cbb5698eade0e16f81
romanusbett <bettromanus@gmail.com>
2026-03-15 12:51:21 +0300

    fix: linting errors

--- apps/vision/src/app/pages/protected-route/protected-route.spec.ts
+++ apps/vision/src/app/pages/protected-route/protected-route.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProtectedRoute } from './protected-route';
+import { provideHttpClient } from '@angular/common/http';
+import { FootballControllerService } from '../../predict_http_api/getAllMatchesApi/services/footballController.service';
+import 'zone.js';
+import 'zone.js/testing';
 
 describe('ProtectedRoute', () => {
   let component: ProtectedRoute;
@@ -8,9 +12,9 @@ describe('ProtectedRoute', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProtectedRoute]
-    })
-    .compileComponents();
+      imports: [ProtectedRoute],
+      providers: [FootballControllerService, provideHttpClient()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProtectedRoute);
     component = fixture.componentInstance;
