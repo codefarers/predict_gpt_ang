6cca1b998954539602f726f2d2ec81e3a8ac596f
romanusbett <bettromanus@gmail.com>
2026-03-24 21:44:34 +0300

    fix tests

--- apps/vision/src/app/pages/matches/views/matches-premier-league/matches-premier-league.spec.ts
+++ apps/vision/src/app/pages/matches/views/matches-premier-league/matches-premier-league.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatchesPremierLeague } from './matches-premier-league';
 
+import 'zone.js';
+import 'zone.js/testing';
+import { provideHttpClient } from '@angular/common/http';
+
+
 describe('MatchesPremierLeague', () => {
   let component: MatchesPremierLeague;
   let fixture: ComponentFixture<MatchesPremierLeague>;
@@ -8,6 +13,9 @@ describe('MatchesPremierLeague', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MatchesPremierLeague],
+      providers: [
+        provideHttpClient()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(MatchesPremierLeague);
